dd6e47ab9cedde82594a0dba1d203ff5a08f6e71
Ocneanu Vlad <ocneanuvlad07@gmail.com>
2025-06-20 17:32:00 +0300

    Co-authored-by: Richard Nsama <kasonde@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
+import { faker } from "@faker-js/faker";
 
 export default {
   /**
@@ -16,5 +17,43 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    // 1. Generate a list of users. (Store their doc IDs in an array after generation by Strapi)
+
+    const randomUsers = faker.helpers.multiple(
+      () => ({
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+      }),
+      { count: 10 }
+    );
+
+    const users = await Promise.all(
+      randomUsers.map((randomUser) =>
+        strapi.documents("plugin::users-permissions.user").create({
+          data: {
+            ...randomUser,
+          },
+        })
+      )
+    );
+
+    // 2. Generate a list of tasks, and randomly assign users from the above array to them
+
+    const randomTasks = faker.helpers.multiple(
+      () => ({
+        user: users[faker.number.int({ min: 0, max: users.length - 1 })],
+        title: faker.lorem.sentence(),
+      }),
+      { count: 1000 }
+    );
+
+    randomTasks.forEach(async (task) => {
+      await strapi.documents("api::task.task").create({
+        data: {
+          ...task,
+        },
+      });
+    });
+  },
 };
